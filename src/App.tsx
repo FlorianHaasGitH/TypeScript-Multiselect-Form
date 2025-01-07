fd9b52d1a969de8faf9129d12b617fb1ddd57095
Florian-Haas <fhaasbusiness@gmail.com>
2025-01-07 18:53:39 +0100

    Set up Container CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Select } from "./components/Select";
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Three", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Five", value: 5 },
+];
+
 function App() {
   return (
     <>
-      <div>
-        <h1>Hi</h1>
-      </div>
+      <Select options={options} />
     </>
   );
 }
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,6 +1,8 @@
+import styles from "../select.module.css";
+
 type SelectOption = {
   label: string;
-  value: string;
+  value: any;
 };
 
 type SelectProps = {
@@ -9,4 +11,19 @@ type SelectProps = {
   onChange: (value: SelectOption | undefined) => void;
 };
 
-export function Select({ value, onChange, options }: SelectProps) {}
+export function Select({ value, onChange, options }: SelectProps) {
+  return (
+    <div tabIndex={0} className={styles.container}>
+      <span className={styles.value}>Value</span>
+      <button className={styles["clear-btn"]}>&times;</button>
+      <div className={styles.caret}></div>
+      <ul className={styles.options}>
+        {options.map((option) => (
+          <li key={option.label} className={styles.option}>
+            {option.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}

72e211ae1f196b29c722a45a30268d67c6eae44f
Florian-Haas <fhaasbusiness@gmail.com>
2025-01-08 18:41:29 +0100

    Set useState for value and css

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -12,12 +12,15 @@ type SelectProps = {
 };
 
 export function Select({ value, onChange, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div tabIndex={0} className={styles.container}>
-      <span className={styles.value}>Value</span>
+      <span className={styles.value}>{value?.label}</span>
       <button className={styles["clear-btn"]}>&times;</button>
+      <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul className={`${styles.option} ${styles.show}`}>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
         {options.map((option) => (
           <li key={option.label} className={styles.option}>
             {option.label}
